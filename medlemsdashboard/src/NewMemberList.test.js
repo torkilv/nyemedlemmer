e5efc07003aaee0fc50ff230d15123d5eee8984f
Torkil Vederhus <torkilv@gmail.com>
2018-10-27 15:20:42 +0200

    Cleaning up

--- medlemsdashboard/src/NewMemberList.test.js
+++ medlemsdashboard/src/NewMemberList.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import NewMemberList from './NewMemberList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<NewMemberList />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
