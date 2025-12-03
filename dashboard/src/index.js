740cced5e817356442fe66c149efe87d645b1612
Torkil Vederhus <torkilv@gmail.com>
2025-12-03 21:18:54 +0100

    Fixed new render call

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import NewMemberList from './NewMemberList';
 
-ReactDOM.render(<NewMemberList />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<NewMemberList />);
